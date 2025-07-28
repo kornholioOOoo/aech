2c8b1f96f654435272424d219cf42f7e2273373e
kornholioOOoo <jejed142@gmail.com>
2025-07-28 22:09:30 +0800

    first push

---
+++ src/juezan/activity3.java
@@ -0,0 +1,24 @@
+
+package juezan;
+
+import java.util.Scanner;
+
+public class activity3 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+        
+        int num;
+        
+        System.out.print("Enter a number: ");
+        num = sc.nextInt();
+        
+        if (num==0){
+            System.out.println("Inputted number is Zero.");
+        }else if (num<0){
+            System.out.println(num+" is a negative number.");
+        }else{
+            System.out.println(num+" is a positive number.");
+        }
+    }
+    
+}
